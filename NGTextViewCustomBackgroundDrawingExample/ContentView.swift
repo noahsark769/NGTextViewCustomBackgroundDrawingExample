4483bd8d5de28c39b824d88d9ecda9adb28ffc74
Noah Gilmore <noah.w.gilmore@gmail.com>
2019-09-26 07:34:36 -0700

    Initial Commit

---
+++ NGTextViewCustomBackgroundDrawingExample/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  NGTextViewCustomBackgroundDrawingExample
+//
+//  Created by Noah Gilmore on 9/26/19.
+//  Copyright © 2019 Noah Gilmore. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
